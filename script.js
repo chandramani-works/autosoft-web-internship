8c6b0ed0a51a8dbcbf7d126306e0b42176e5947a
Chandramani <chandramani@gmail.com>
2026-09-11 00:08:39 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector(".btn");
+
+    button.addEventListener("click", function () {
+        alert("Welcome to AutoSoft Web Internship!");
+    });
+});
